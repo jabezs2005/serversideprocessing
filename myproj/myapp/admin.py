6f0b6fde33be8241167259f89a5f22498bf57ac6
jabezs2005 <jabsudha2005@gmail.com>
2024-01-02 08:35:48 +0530

    first commit

---
+++ myproj/myapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('areaofrectangle/',views.rectarea,name="areaofrectangle"),
+]
+
+
+"""path('',views.rectarea,name="areaofrectangleroot")"""
